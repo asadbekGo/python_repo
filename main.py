a0a82a70dfaaa09b91e35e3e9ab60a06dc155924
asadbek <asadbek30665@gmail.com>
2022-08-18 20:28:20 +0500

    future calculate function

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 
-print("hello gitlab")
+
+def sum(a, b):
+    return a + b
+
+def substraction(a, b):
+    return a - b
+
+
+def division(a, b):
+    return a / b
+
+
+def multiple(a, b):
+    return a * b
+
+
+
+
+print(sum(3, 5))
+
